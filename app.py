459182928345898babcc392898199fa36da21220
Camille Brodin <camillebrodin@outlook.fr>
2026-03-06 16:50:22 +0100

    06032026

--- app.py
+++ app.py
@@ -1,41 +1,24 @@
-import os
 import streamlit as st
 from PIL import Image
-import io
-import time
-from huggingface_hub import InferenceClient
 from transformers import pipeline
 
+pipe = pipeline("image-classification", model="chriamue/bird-species-classifier")
 
-client = InferenceClient(
-    provider="auto",
-    api_key=os.getenv("HF_TOKEN"),
-)
+st.title("Bird Species Classifier")
 
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg","jpeg","png"])
 
-def app():
-    st.title("Bird Species Classifier")
+if uploaded_file is not None:
 
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    image = Image.open(uploaded_file)
 
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", width=300)
 
-        buffer = io.BytesIO()
-        image.save(buffer, format="JPEG")
+    result = pipe(image)
 
-        st.image(image, caption="Uploaded Image.", width=300)
+    top = result[0]
 
-        
-        pipe = pipeline("image-classification", model="chriamue/bird-species-classifier")
-        result = pipe(image)
+    label = top["label"]
+    score_percent = top["score"] * 100
 
-        top = result[0]
-
-        label = top["label"]
-        score_percent = top["score"] * 100
-
-        st.write("Predictions:", f"{label} ==> {score_percent:.2f}%")
-
-if __name__ == "__main__":
-    app()
+    st.write(f"{label} ==> {score_percent:.2f}%")
